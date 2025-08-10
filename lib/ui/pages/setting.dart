174db631f5590a4269fd3f0659cf9a44a8d0e305
Victor <vmquintanasnow@gmail.com>
2025-08-10 18:02:23 +0300

    Add settings improvements

--- lib/ui/pages/setting.dart
+++ lib/ui/pages/setting.dart
@@ -1,12 +1,32 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:path_provider/path_provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const ScaffoldPage(
-      header: PageHeader(title: Text('Settings')),
-      content: Center(child: Text('Nothing here yet.')),
+    return ScaffoldPage(
+      header: const PageHeader(title: Text('Settings')),
+      content: FutureBuilder<String>(
+        future: _getLedgerPath(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                Text('Ubicación de los datos:'),
+                Text(snapshot.data ?? ''),
+              ],
+            );
+          } else {
+            return const Center(child: Text('Nothing here yet.'));
+          }
+        },
+      ),
     );
+  }
+
+  Future<String> _getLedgerPath() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return '${dir.path}/ledger.json';
   }
 }
